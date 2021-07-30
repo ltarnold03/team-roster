565c09d423090943b74f81f48e76ab74bf4c5b11
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-07-30 00:09:51 -0500

    Manager test pass

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -2,9 +2,13 @@
 const Manager = require('../lib/Manager.js');
 
     test('checks if the Manager information is returned', () => {
-        const manager = new Manager();
+        const manager = new Manager('Bambi Deer', '012', 'bambi@email.com', '1234');
 
-        expect(manager.officeNumber).toEqual(expect.any(Number));
-        expect(manager.officeNumber.length).toBeGreaterThan(0);
+        expect(manager.officeNumber).toEqual(expect.stringContaining('1234'));
     });
     
+    test('checks if Manager role is returned', () => {
+        const manager = new Manager('Bambi Deer', '012', 'bambi@email.com');
+     
+        expect(manager.getRole()).toEqual('Manager');
+     });
